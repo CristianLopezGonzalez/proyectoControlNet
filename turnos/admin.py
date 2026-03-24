27d29c343ea4574a5c97760a9e1be42df5f4f089
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-03-24 20:04:02 +0100

    fix

--- turnos/admin.py
+++ turnos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CalendarioSemanal, AsignacionTarde
 
-# Register your models here.
+@admin.register(CalendarioSemanal)
+class CalendarioSemanalAdmin(admin.ModelAdmin):
+    list_display = ('anio', 'numero_semana', 'fecha_inicio_semana', 'fecha_fin_semana', 'estado')
+    list_filter = ('estado', 'anio')
+
+@admin.register(AsignacionTarde)
+class AsignacionTardeAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'semana', 'dia', 'hora_inicio', 'hora_fin', 'estado')
+    list_filter = ('dia', 'estado')
